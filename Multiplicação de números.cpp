3522464a5521a71e5864338937e2494af2486b88
Anna <abgtravascio@gmail.com>
2025-05-06 14:58:53 -0300

    Create Multiplicação de números.cpp

---
+++ Multiplicação de números.cpp
@@ -0,0 +1,12 @@
+//https://neps.academy/br/exercise/1957
+#include <iostream>
+#define int long long
+using namespace std;
+
+signed main(){
+    int a, b;
+    cin>>a>>b;
+    cout<<a*b<<endl;
+
+    return 0;
+}
